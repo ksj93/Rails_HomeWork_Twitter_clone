049c9af6615a37c145f41a92f21175604647e1d9
kim sungjoo <ksjee17@gmail.com>
2021-10-01 06:07:52 +0000

    add input_validation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,10 +6,23 @@ class PostsController < ApplicationController
     @post = Post.new
   end
   def create
-    @post =Post.create(content: params[:post][:content])
-    render:new
+    @post =Post.create(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "投稿完了"
+    else
+      render:new
+    end
   end
   def show
     @post = Post.find(params[:id])
+  end
+  def edit
+
+  end
+  def destroy
+  end
+  private
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
